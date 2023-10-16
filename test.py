0482b8f61669c94ab02b3a9aa1d6a1c57b263aeb
Wang Chen <lolitilol12@gmail.com>
2023-10-16 18:42:31 +0800

    Test

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 
 print("Hello World")
-a = input("Give me a number: ")
-print(str(a))
-b = a +1
-print(str(b))
+b = input("Give me a number: ")
+
+
+for i in range (b+1):
+    if(i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0):
+        print(i)
+
+#Waffles
+
+
